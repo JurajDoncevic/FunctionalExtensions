afc81b4ca65510c13118638013f1285e83928519
JurajDoncevic <Juraj@BELINA>
2021-02-18 19:50:33 +0100

    Tests for DataResult and Result

---
+++ FunctionalExtensions/FunctionalExtensions.Tests/DataResultTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FunctionalExtensions.Base.Results;
+using FunctionalExtensions.Tests.Data;
+using static FunctionalExtensions.Base.Try;
+using Xunit;
+
+namespace FunctionalExtensions.Tests
+{
+    public class DataResultTests
+    {
+        private readonly NorthwindDbContext _ctx;
+
+        public DataResultTests(NorthwindDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [Fact]
+        public void DataResultSuccess()
+        {
+            var dataResult =
+                TryCatch(
+                    () => new { Name = "test" },
+                    (ex) => ex
+                    ).ToDataResult();
+
+            Assert.NotNull(dataResult);
+            Assert.True(dataResult.IsSuccess);
+            Assert.False(dataResult.IsFailure);
+            Assert.Equal("test", dataResult.Data.Name);
+        }
+
+        [Fact]
+        public void DataResultExceptionFail()
+        {
+            const string exceptionMessage = "Exception message";
+
+            var dataResult =
+                TryCatch(
+                    () => { throw new Exception(exceptionMessage); return new { Name = "test" }; },
+                    (ex) => ex
+                    ).ToDataResult();
+
+            Assert.NotNull(dataResult);
+            Assert.False(dataResult.IsSuccess);
+            Assert.True(dataResult.IsFailure);
+            Assert.Equal(exceptionMessage, dataResult.ErrorMessage);
+            Assert.Equal(ErrorType.ExceptionThrown, dataResult.ErrorType);
+        }
+    }
+}
---
+++ FunctionalExtensions/FunctionalExtensions.Tests/ResultTests.cs
@@ -0,0 +1,90 @@
+﻿using FunctionalExtensions.Tests.Data;
+using static FunctionalExtensions.Base.Try;
+using static FunctionalExtensions.Base.ActionExtensions;
+using static FunctionalExtensions.Base.FunctionalHelpers;
+using FunctionalExtensions.Base.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FunctionalExtensions.Base;
+
+namespace FunctionalExtensions.Tests
+{
+    public class ResultTests
+    {
+        private readonly NorthwindDbContext _ctx;
+
+        public ResultTests(NorthwindDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [Fact]
+        public void ResultSuccess()
+        {
+            var result =
+                TryCatch(
+                    ((Action)(() => Console.WriteLine())).ToFunc(),
+                    (ex) => ex
+                    ).ToResult();
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.False(result.IsFailure);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public void ResultExceptionFail()
+        {
+            const string exceptionMessage = "Exception message";
+
+            var result =
+                TryCatch(
+                    ((Action)(() => {throw new Exception(exceptionMessage); Console.WriteLine(); })).ToFunc(),
+                    (ex) => ex
+                    ).ToResult();
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorType.ExceptionThrown, result.ErrorType);
+        }
+
+        [Fact]
+        public void ResultLogicSuccess()
+        {
+            const string exceptionMessage = "Exception message";
+
+            var result =
+                TryCatch(
+                    () => true,
+                    (ex) => ex
+                    ).ToResult();
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.False(result.IsFailure);
+            Assert.Equal(string.Empty, result.ErrorMessage);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public void ResultLogicFail()
+        {
+
+            var result =
+                TryCatch(
+                    () => false,
+                    (ex) => ex
+                    ).ToResult();
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal("Operation failed", result.ErrorMessage);
+            Assert.Equal(ErrorType.Failure, result.ErrorType);
+        }
+    }
+}
