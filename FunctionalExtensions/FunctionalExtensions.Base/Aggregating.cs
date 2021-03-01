80e78d4e3dd2102282c605489faf6bc38b974f5b
JurajDoncevic <juraj.doncevic@fer.hr>
2021-03-01 11:59:58 +0100

    Fold function added

---
+++ FunctionalExtensions/FunctionalExtensions.Base/Aggregating.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtensions.Base
+{
+    public static class Aggregating
+    {
+        /// <summary>
+        /// Used to fold a IEnumerable into a single value.
+        /// E[T] -> R -> (T -> R -> R) -> R
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="ts">Target enumerable</param>
+        /// <param name="initalizer">Initial value</param>
+        /// <param name="foldingFunc">Accumulation function</param>
+        /// <returns></returns>
+        public static R Fold<T, R>(this IEnumerable<T> ts, R seed, Func<T, R, R> foldingFunc)
+        {
+            R result = seed;
+            foreach (T item in ts)
+            {
+                result = foldingFunc(item, result);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Used to async fold a IEnumerable into a single value.
+        /// E[T] -> R -> (T -> R -> R) -> R
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="ts">Target enumerable</param>
+        /// <param name="initalizer">Initial value</param>
+        /// <param name="foldingFunc">Accumulation function</param>
+        /// <returns></returns>
+        public static async Task<R> Fold<T, R>(this Task<IEnumerable<T>> ts, R seed, Func<T, R, R> foldingFunc)
+        {
+            R result = seed;
+            foreach (T item in await ts)
+            {
+                result = foldingFunc(item, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FunctionalExtensions/FunctionalExtensions.Tests/Tests/BaseTests.cs
@@ -0,0 +1,40 @@
+﻿using FunctionalExtensions.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace FunctionalExtensions.Tests
+{
+    public class BaseTests
+    {
+        [Fact]
+        public static void FoldOnSameTypesTest()
+        {
+            const string testCsv = "This;is;some;rediculous;Csv;line";
+
+            var csvSplit = testCsv.Split(';').ToList();
+
+            string finalCsv = csvSplit.Skip(1).Fold(csvSplit.First(), (item, seed) => seed + ";" + item);
+
+            Assert.NotNull(finalCsv);
+            Assert.NotEmpty(finalCsv);
+            Assert.Equal(testCsv, finalCsv);
+        }
+
+        [Fact]
+        public static void FoldOnDifferentTypesTest()
+        {
+            const string testString = "12345";
+
+            List<int> testInts = new List<int>{ 1, 2, 3, 4, 5 };
+
+            string finalCsv = testInts.Skip(1).Fold(testInts.First().ToString(), (item, seed) => seed + item.ToString());
+
+            Assert.NotNull(finalCsv);
+            Assert.NotEmpty(finalCsv);
+            Assert.Equal(testString, finalCsv);
+        }
+    }
+}
