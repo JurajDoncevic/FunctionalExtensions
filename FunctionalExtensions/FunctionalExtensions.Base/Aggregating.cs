900d0bd4e3f20f636cb508cebde2970fd8492b1c
JurajDoncevic <Juraj@BELINA>
2021-03-16 13:17:37 +0100

    Foldi extension method

--- FunctionalExtensions/FunctionalExtensions.Base/Aggregating.cs
+++ FunctionalExtensions/FunctionalExtensions.Base/Aggregating.cs
@@ -48,5 +48,28 @@ namespace FunctionalExtensions.Base
 
             return result;
         }
+
+        /// <summary>
+        /// Used to async fold a IEnumerable into a single value with element index as parameter to folding func.
+        /// E[T] -> R -> (T -> R -> R) -> R
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="ts">Target enumerable</param>
+        /// <param name="initalizer">Initial value</param>
+        /// <param name="foldingFunc">Accumulation function</param>
+        /// <returns></returns>
+        public static async Task<R> Foldi<T, R>(this Task<IEnumerable<T>> ts, R seed, Func<long, T, R, R> foldingFunc)
+        {
+            R result = seed;
+            long idx = 0;
+            foreach (T item in await ts)
+            {
+                result = foldingFunc(idx, item, result);
+                idx++;
+            }
+
+            return result;
+        }
     }
 }
