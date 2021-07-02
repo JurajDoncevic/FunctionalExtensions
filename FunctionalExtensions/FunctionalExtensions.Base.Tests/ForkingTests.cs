ec66e6fbef9ed8c909b93171de74f95bade10964
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2021-07-02 21:19:38 +0200

    Forking methods refactored, added and tests added

---
+++ FunctionalExtensions/FunctionalExtensions.Base.Tests/ForkingTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace FunctionalExtensions.Base.Tests
+{
+    public class ForkingTests
+    {
+        [Fact]
+        public void ForkSimpleTest()
+        {
+            int x = 0;
+            var result =
+                x.Fork(
+                    _ => _.Sum(),
+                    _ => _ + 1,
+                    _ => _ + 2,
+                    _ => _ + 3
+                    );
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public async void ForkAsyncFuncsTest()
+        {
+            int x = 0;
+            var result =
+                await
+                    x.Fork(
+                        _ => _.Sum(),
+                        async _ => await Task.FromResult(_ + 1),
+                        async _ => await Task.FromResult(_ + 2),
+                        async _ => await Task.FromResult(_ + 3)
+                        );
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public async void ForkFullAsyncFuncsTest()
+        {
+            int x = 0;
+            var result =
+                await
+                    x.Fork(
+                        async _ => (await Task.WhenAll(_)).Sum(),
+                        async _ => await Task.FromResult(_ + 1),
+                        async _ => await Task.FromResult(_ + 2),
+                        async _ => await Task.FromResult(_ + 3)
+                        );
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public async void ForkAsyncNoParamFuncsTest()
+        {
+            var result =
+                await
+                    Forking.Fork(
+                        _ => _.Sum(),
+                        async () => await Task.FromResult(1),
+                        async () => await Task.FromResult(2),
+                        async () => await Task.FromResult(3)
+                        );
+
+            Assert.Equal(6, result);
+        }
+    }
+}
--- FunctionalExtensions/FunctionalExtensions.Base/Forking.cs
+++ FunctionalExtensions/FunctionalExtensions.Base/Forking.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunctionalExtensions.Base
 {
@@ -16,20 +17,66 @@ namespace FunctionalExtensions.Base
         /// <param name="finalizeFunc">Finalizing function</param>
         /// <param name="prongs">Prong function</param>
         /// <returns></returns>
-        public static TOutput Fork<TInput, TOutput>(this TInput target, Func<IEnumerable<TOutput>, TOutput> finalizeFunc,
-                                                    params Func<TInput, TOutput>[] prongs) =>
-            prongs.AsParallel().Select(_ => _(target)).Map(finalizeFunc);
+        public static TOutput Fork<TInput, TProngOutput, TOutput>(this TInput target, Func<IEnumerable<TProngOutput>, TOutput> finalizeFunc,
+                                                    params Func<TInput, TProngOutput>[] prongs)
+            => prongs.AsParallel().Map(_ => _(target)).Map(finalizeFunc);
 
         /// <summary>
         /// Executes the prong functions and finalizes the result with the finalize function. Used to aggergate multiple function results.
+        /// </summary>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="finalizeFunc">Finalizing function</param>
+        /// <param name="prongs">Prong
+        public static TOutput Fork<TProngOutput, TOutput>(Func<IEnumerable<TProngOutput>, TOutput> finalizeFunc, params Func<TProngOutput>[] prongs)
+            => prongs.AsParallel().Map(_ => _.Invoke()).Map(finalizeFunc);
+
+
+        /// <summary>
+        /// Executes the prong functions and finalizes the result with the finalize function. Used to aggergate multiple function results.
+        /// </summary>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="finalizeFunc">Finalizing function</param>
+        /// <param name="prongs">Prong
+        public static async Task<TOutput> Fork<TProngOutput, TOutput>(Func<IEnumerable<Task<TProngOutput>>, Task<TOutput>> finalizeFunc, params Func<Task<TProngOutput>>[] prongs)
+            => await prongs.Map(async _ => await _())
+                           .Map(finalizeFunc);
+
+        /// <summary>
+        /// Executes the prong functions and finalizes the result with the finalize function. Used to aggergate multiple function results.
+        /// Sync finalizing function. Use when the finalize function is small.
+        /// </summary>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="finalizeFunc">Finalizing function</param>
+        /// <param name="prongs">Prong
+        public static async Task<TOutput> Fork<TProngOutput, TOutput>(Func<IEnumerable<TProngOutput>, TOutput> finalizeFunc, params Func<Task<TProngOutput>>[] prongs)
+            => (await Task.WhenAll(prongs.Map(async _ => await _())))
+                    .Map(finalizeFunc);
+
+        /// <summary>
+        /// Executes the prong functions and finalizes the result with the finalize function. Used to aggergate multiple function results.
+        /// Fully async
         /// </summary>
         /// <typeparam name="TInput"></typeparam>
         /// <typeparam name="TOutput"></typeparam>
         /// <param name="target">Target object</param>
         /// <param name="finalizeFunc">Finalizing function</param>
         /// <param name="prongs">Prong
-        public static TOutput Fork<TOutput>(Func<IEnumerable<TOutput>, TOutput> finalizeFunc,
-                                                    params Func<TOutput>[] prongs) =>
-            prongs.AsParallel().Select(_ => _.Invoke()).Map(finalizeFunc);
+        public static async Task<TOutput> Fork<TInput, TProngOutput, TOutput>(this TInput target, Func<IEnumerable<Task<TProngOutput>>, Task<TOutput>> finalizeFunc, params Func<TInput, Task<TProngOutput>>[] prongs)
+            => await prongs.Map(async _ => await _(target))
+                           .Map(finalizeFunc);
+
+
+        /// <summary>
+        /// Executes the prong functions and finalizes the result with the finalize function. Used to aggergate multiple function results.
+        /// Sync finalizing function. Use when the finalize function is small.
+        /// </summary>
+        /// <typeparam name="TInput"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="target">Target object</param>
+        /// <param name="finalizeFunc">Finalizing function</param>
+        /// <param name="prongs">Prong
+        public static async Task<TOutput> Fork<TInput, TProngOutput, TOutput>(this TInput target, Func<IEnumerable<TProngOutput>, TOutput> finalizeFunc, params Func<TInput, Task<TProngOutput>>[] prongs)
+            => (await Task.WhenAll(prongs.Map(async _ => await _(target))))
+                    .Map(finalizeFunc);
     }
 }
