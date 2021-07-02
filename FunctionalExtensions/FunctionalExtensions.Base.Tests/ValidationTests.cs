c5b5d3369e5d2da33a959e1bc6434edc62ffe825
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2021-07-02 22:25:30 +0200

    Validation tests added; Validator created

---
+++ FunctionalExtensions/FunctionalExtensions.Base.Tests/ValidationTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FunctionalExtensions.Base.Tests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void ValidationPassTest()
+        {
+            var person = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 24
+            };
+
+            var result = person.Validate(
+                _ => _.FirstName.Length > 2,
+                _ => _.LastName.Length > 2,
+                _ => _.Age > 18
+                );
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ValidationFailTest()
+        {
+            var person = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 16
+            };
+
+            var result = person.Validate(
+                _ => _.FirstName.Length > 2,
+                _ => _.LastName.Length > 2,
+                _ => _.Age > 18
+                );
+
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void ValidatorTest()
+        {
+            var person1 = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 24
+            };
+
+            var person2 = new Person
+            {
+                FirstName = "Joe",
+                LastName = "Smith",
+                Age = 18
+            };
+
+            var validator = 
+                Validation.Validator<Person>(
+                    _ => _.FirstName.Length > 2,
+                    _ => _.LastName.Length > 2,
+                    _ => _.Age > 18
+                    );
+
+            var result1 = validator(person1);
+            var result2 = validator(person2);
+
+            Assert.True(result1);
+            Assert.False(result2);
+        }
+
+        private class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
--- FunctionalExtensions/FunctionalExtensions.Base/Validation.cs
+++ FunctionalExtensions/FunctionalExtensions.Base/Validation.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunctionalExtensions.Base
 {
     public static class Validation
     {
-        /*Inspired by Simon Painter*/
         /// <summary>
         /// Checks if the given input object is valid by all the given predicates
         /// </summary>
@@ -15,7 +15,43 @@ namespace FunctionalExtensions.Base
         /// <param name="target">Target object that is to be validated</param>
         /// <param name="predicates">Predicates to check validity</param>
         /// <returns>True if passes predicates, else false</returns>
-        public static bool Validate<TInput>(this TInput target, params Func<TInput, bool>[] predicates) =>
-            predicates.AsParallel().All(_ => _.Invoke(target));
+        public static bool Validate<TInput>(this TInput target, params Func<TInput, bool>[] predicates) 
+            => predicates.AsParallel().All(_ => _(target));
+
+
+        /// <summary>
+        /// Checks if the given input object is valid by all the given predicates
+        /// </summary>
+        /// <typeparam name="TInput">Type of the input object</typeparam>
+        /// <param name="target">Target object that is to be validated</param>
+        /// <param name="predicates">Predicates to check validity</param>
+        /// <returns>True if passes predicates, else false</returns>
+        public static async Task<bool> Validate<TInput>(this TInput target, params Func<TInput, Task<bool>>[] predicates) 
+            => (await Task.WhenAll(predicates.Map(async _ => await _(target))))
+                    .All(_ => _);
+
+
+        /// <summary>
+        /// Creates a validator function
+        /// </summary>
+        /// <typeparam name="TInput">Type of the input object</typeparam>
+        /// <param name="target">Target object that is to be validated</param>
+        /// <param name="predicates">Predicates to check validity</param>
+        /// <returns>True if passes predicates, else false</returns>
+        public static Func<TInput, bool> Validator<TInput>(params Func<TInput, bool>[] predicates) 
+            => (TInput target) 
+                => predicates.AsParallel().All(_ => _(target));
+
+        /// <summary>
+        /// Creates a validator function
+        /// </summary>
+        /// <typeparam name="TInput">Type of the input object</typeparam>
+        /// <param name="target">Target object that is to be validated</param>
+        /// <param name="predicates">Predicates to check validity</param>
+        /// <returns>True if passes predicates, else false</returns>
+        public static Func<TInput, Task<bool>> Validator<TInput>(params Func<TInput, Task<bool>>[] predicates) 
+            => async (TInput target) 
+                => (await Task.WhenAll(predicates.Map(async _ => await _(target))))
+                    .All(_ => _);
     }
 }
