96e923df3566e60fd35501db81a2ff6414a732a1
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2021-07-02 19:03:57 +0200

    Awaiting added

--- FunctionalExtensions/FunctionalExtensions.Base/Awaiting.cs
+++ FunctionalExtensions/FunctionalExtensions.Base/Awaiting.cs
@@ -8,15 +8,31 @@ namespace FunctionalExtensions.Base
     public static class Awaiting
     {
         /// <summary>
-        /// Extension method for awating a task result and operating over it. Blocks thread!
+        /// Awaits a task result and operates over it. Blocks thread!
         /// Uses Task.Result call - not to be used lightly!
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <typeparam name="R"></typeparam>
         /// <param name="task"></param>
-        /// <param name="operation"></param>
+        /// <param name="operation">Operation to execute over result</param>
         /// <returns>Sync result of operation</returns>
         public static R WaitFor<T, R>(this Task<T> task, Func<T, R> operation)
             => operation(task.Result);
+
+        /// <summary>
+        /// Awaits a task result until timeout and operates over it if finished. Blocks thread!
+        /// Uses Task.Result call - not to be used lightly!
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="operation">Operation to execute over result</param>
+        /// <param name="timeoutMillis"></param>
+        /// <returns>Sync result of operation or default value of return type</returns>
+        public static R WaitFor<T, R>(this Task<T> task, Func<T, R> operation, int timeoutMillis)
+            => task.Wait(timeoutMillis)
+                ? operation(task.Result)
+                : default(R);
+
     }
 }
