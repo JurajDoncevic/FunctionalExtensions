3d57ce0fb8d19c8ef9f6c0b78868226996daf600
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2021-07-03 19:31:34 +0200

    DataResult and Result Bind exchange

---
+++ FunctionalExtensions/FunctionalExtensions.Base/Results/CommonResultExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtensions.Base.Results
+{
+    /// <summary>
+    /// Extensions methods for public creation of DataResult and Result
+    /// </summary>
+    public static partial class ResultExtensions
+    {
+        /// <summary>
+        /// Used to pipe Result objects. 
+        /// R[] -> (bool -> DR[T]) -> DR[T]
+        /// </summary>
+        /// <param name="target">Original result</param>
+        /// <param name="func">Function to possibly construct next result in sequence</param>
+        /// <returns></returns>
+        public static DataResult<T> Bind<T>(this Result target, Func<bool, DataResult<T>> func) =>
+            target.IsSuccess
+            ? func(target.IsSuccess)
+            : target.Map(_ => new DataResult<T>(false, target.ErrorMessage, target.ErrorType));
+
+
+        /// <summary>
+        /// Used to pipe Result objects.
+        /// R[] -> (bool -> DR[T]) -> DR[T]
+        /// </summary>
+        /// <param name="target">Original result</param>
+        /// <param name="func">Function to possibly construct next result in sequence</param>
+        /// <returns></returns>
+        public static async Task<DataResult<T>> Bind<T>(this Task<Result> target, Func<bool, Task<DataResult<T>>> func) =>
+            (await target) switch
+            {
+                Result { IsSuccess: true } result => await func(result.IsSuccess),
+                Result { IsSuccess: false } result => result.Map(_ => new DataResult<T>(false, _.ErrorMessage, _.ErrorType))
+            };
+
+        /// <summary>
+        /// Used to pipe Result objects. 
+        /// DR[T] -> (DR[T] -> R[]) -> R[]
+        /// </summary>
+        /// <param name="target">Original result</param>
+        /// <param name="func">Function to possibly construct next result in sequence</param>
+        /// <returns></returns>
+        public static Result Bind<T>(this DataResult<T> target, Func<DataResult<T>, Result> func) =>
+            target.IsSuccess
+            ? func(target)
+            : target.Map(_ => new Result(false, _.ErrorMessage, _.ErrorType));
+
+
+        /// <summary>
+        /// Used to pipe Result objects.
+        /// R[] -> (bool -> DR[T]) -> DR[T]
+        /// </summary>
+        /// <param name="target">Original result</param>
+        /// <param name="func">Function to possibly construct next result in sequence</param>
+        /// <returns></returns>
+        public static async Task<Result> Bind<T>(this Task<DataResult<T>> target, Func<DataResult<T>, Task<Result>> func) =>
+            (await target) switch
+            {
+                DataResult<T> { IsSuccess: true } result => await func(result),
+                DataResult<T> { IsSuccess: false } result => result.Map(_ => new Result(false, _.ErrorMessage, _.ErrorType))
+            };
+    }
+}
