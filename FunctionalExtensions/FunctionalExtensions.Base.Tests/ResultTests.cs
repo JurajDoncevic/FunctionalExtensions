7edc16287f821e636ed641e7dfe4448fc068a7f9
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2021-08-27 15:50:25 +0200

    Bind tests added

--- FunctionalExtensions/FunctionalExtensions.Base.Tests/ResultTests.cs
+++ FunctionalExtensions/FunctionalExtensions.Base.Tests/ResultTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using FunctionalExtensions.Base;
+using System.Threading.Tasks;
 
 namespace FunctionalExtensions.Base.Tests
 {
@@ -14,7 +15,7 @@ namespace FunctionalExtensions.Base.Tests
     {
 
         [Fact]
-        public void ResultSuccess()
+        public void ResultSuccessTest()
         {
             var result =
                 TryCatch(
@@ -29,13 +30,13 @@ namespace FunctionalExtensions.Base.Tests
         }
 
         [Fact]
-        public void ResultExceptionFail()
+        public void ResultExceptionFailTest()
         {
             const string exceptionMessage = "Exception message";
 
             var result =
                 TryCatch(
-                    ((Action)(() => {throw new Exception(exceptionMessage); Console.WriteLine(); })).ToFunc(),
+                    ((Action)(() => {throw new Exception(exceptionMessage); })).ToFunc(),
                     (ex) => ex
                     ).ToResult();
 
@@ -46,11 +47,11 @@ namespace FunctionalExtensions.Base.Tests
         }
 
         [Fact]
-        public void ResultTypedExceptionFail()
+        public void ResultTypedExceptionFailTest()
         {
             var result =
                 TryCatch(
-                    ((Action)(() => { throw new ArgumentNullException(); Console.WriteLine(); })).ToFunc(),
+                    ((Action)(() => { throw new ArgumentNullException(); })).ToFunc(),
                     (ex) => ex
                     ).ToResult();
 
@@ -62,7 +63,7 @@ namespace FunctionalExtensions.Base.Tests
         }
 
         [Fact]
-        public void ResultLogicSuccess()
+        public void ResultLogicSuccessTest()
         {
             const string exceptionMessage = "Exception message";
 
@@ -80,7 +81,7 @@ namespace FunctionalExtensions.Base.Tests
         }
 
         [Fact]
-        public void ResultLogicFail()
+        public void ResultLogicFailTest()
         {
 
             var result =
@@ -95,5 +96,149 @@ namespace FunctionalExtensions.Base.Tests
             Assert.Equal("Operation failed", result.ErrorMessage);
             Assert.Equal(ErrorTypes.Failure, result.ErrorType);
         }
+
+        [Fact]
+        public void ResultBindSuccessTest()
+        {
+            var result =
+                GetLogicSuccess()
+                .Bind(_ => GetSuccessResult())
+                .Bind(_ => GetLogicSuccess())
+                .Bind(_ => GetSuccessResult());
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.False(result.IsFailure);
+            Assert.Equal(ErrorTypes.None, result.ErrorType);
+        }
+
+        [Fact]
+        public void ResultBindLogicFailTest()
+        {
+            var result =
+                GetLogicSuccess()
+                .Bind(_ => GetSuccessResult())
+                .Bind(_ => GetLogicFail())
+                .Bind(_ => GetSuccessResult());
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorTypes.Failure, result.ErrorType);
+        }
+
+        [Fact]
+        public void ResultBindExceptionFailTest()
+        {
+            string exceptionMessage = "EXCEPTION MESSAGE";
+            var result =
+                GetLogicSuccess()
+                    .Bind(_ => GetSuccessResult())
+                    .Bind(_ => GetExceptionFail(exceptionMessage))
+                    .Bind(_ => GetSuccessResult());
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorTypes.ExceptionThrown, result.ErrorType);
+            Assert.Equal(exceptionMessage, result.ErrorMessage);
+        }
+
+        [Fact]
+        public async void ResultBindSuccessAsyncTest()
+        {
+            var result =
+                await GetLogicSuccessAsync()
+                    .Bind(_ => GetSuccessResultAsync())
+                    .Bind(_ => GetLogicSuccessAsync())
+                    .Bind(_ => GetSuccessResultAsync());
+
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.False(result.IsFailure);
+            Assert.Equal(ErrorTypes.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async void ResultBindLogicFailAsyncTest()
+        {
+            var result =
+                await GetLogicSuccessAsync()
+                    .Bind(_ => GetSuccessResultAsync())
+                    .Bind(_ => GetLogicFailAsync())
+                    .Bind(_ => GetSuccessResultAsync());
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorTypes.Failure, result.ErrorType);
+        }
+
+        [Fact]
+        public async void ResultBindExceptionFailAsyncTest()
+        {
+            string exceptionMessage = "EXCEPTION MESSAGE";
+            var result =
+                await GetLogicSuccessAsync()
+                    .Bind(_ => GetSuccessResultAsync())
+                    .Bind(_ => GetExceptionFailAsync(exceptionMessage))
+                    .Bind(_ => GetSuccessResultAsync());
+
+            Assert.NotNull(result);
+            Assert.False(result.IsSuccess);
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorTypes.ExceptionThrown, result.ErrorType);
+            Assert.Equal(exceptionMessage, result.ErrorMessage);
+        }
+
+        #region HELPER METHODS
+        private Result GetLogicFail()
+            => TryCatch(
+                    () => false,
+                    (ex) => ex
+                    ).ToResult();
+
+        private Result GetLogicSuccess()
+            => TryCatch(
+                    () => true,
+                    (ex) => ex
+                    ).ToResult();
+
+        private Result GetSuccessResult()
+            => TryCatch(
+                    ((Action)(() => Console.WriteLine())).ToFunc(),
+                    (ex) => ex
+                    ).ToResult();
+
+        private Result GetExceptionFail(string exceptionMessage)
+            => TryCatch(
+                    ((Action)(() => { throw new Exception(exceptionMessage); })).ToFunc(),
+                    (ex) => ex
+                    ).ToResult();
+
+        private async Task<Result> GetLogicFailAsync()
+            => await TryCatch(
+                    async () => { await Task.Run(() => 0); return false; },
+                    (ex) => ex
+                    ).ToResult();
+
+        private async Task<Result> GetLogicSuccessAsync()
+            => await TryCatch(
+                    async () => { await Task.Run(() => 0); return true; },
+                    (ex) => ex
+                    ).ToResult();
+
+        private async Task<Result> GetSuccessResultAsync()
+            => await TryCatch(
+                    async () => (await Task.Run(() => 0)).Ignore(),
+                    (ex) => ex
+                    ).ToResult();
+
+        private async Task<Result> GetExceptionFailAsync(string exceptionMessage)
+            => await TryCatch(
+                    async () => { throw new Exception(exceptionMessage); return await Task.Run(() => Unit()); },
+                    (ex) => ex
+                    ).ToResult();
+        #endregion
     }
 }
