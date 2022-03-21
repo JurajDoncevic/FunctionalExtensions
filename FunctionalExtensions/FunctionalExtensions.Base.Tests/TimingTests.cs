fd4eff79de64536e132a0c526eeebfa444cb6167
JurajDoncevic <juraj.doncevic@fer.hr>
2022-03-21 16:18:24 +0100

    Added Timing methods for executing operations with timeouts

---
+++ FunctionalExtensions/FunctionalExtensions.Base.Tests/TimingTests.cs
@@ -0,0 +1,190 @@
+﻿using FunctionalExtensions.Base.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalExtensions.Base.Tests
+{
+    public class TimingTests
+    {
+        [Fact]
+        public void TestBlockingDataResultTimeout()
+        {
+            Func<int> shortOp = () => { Task.Delay(200).Wait(); return 200; };
+            Func<int> longOp = () => { Task.Delay(700).Wait(); return 700; };
+
+            var shortOpResult = shortOp.RunWithTimeout(500);
+            var longOpResult = longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+            Assert.True(shortOpResult.HasData);
+            Assert.Equal(200, shortOpResult.Data);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void TestAsyncOperationTimeout()
+        {
+            Func<Task<int>> shortOp = async () => { await Task.Delay(200); return 200; };
+            Func<Task<int>> longOp = async () => { await Task.Delay(700); return 700; };
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+            Assert.True(shortOpResult.HasData);
+            Assert.Equal(200, shortOpResult.Data);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void TestDataResultTimeout()
+        {
+            Func<DataResult<int>> shortOp = () => ResultExtensions.AsDataResult(() =>
+            {
+                Task.Delay(200).Wait(); 
+                return 200;
+            });
+            Func<DataResult<int>> longOp = () => ResultExtensions.AsDataResult(() =>
+            {
+                Task.Delay(700).Wait();
+                return 200;
+            });
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+            Assert.True(shortOpResult.HasData);
+            Assert.Equal(200, shortOpResult.Data);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void TestAsyncOperationDataResultTimeout()
+        {
+            var shortOp = async () => await ResultExtensions.AsDataResult(
+                async () =>
+            {
+                await Task.Delay(200);
+                return 200;
+            });
+            var longOp = async () => await ResultExtensions.AsDataResult(
+                async () =>
+            {
+                await Task.Delay(700);
+                return 200;
+            });
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+            Assert.True(shortOpResult.HasData);
+            Assert.Equal(200, shortOpResult.Data);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+
+        [Fact]
+        public void TestOperationBoolResultTimeout()
+        {
+            var shortOp = () =>
+                {
+                    Task.Delay(200).Wait();
+                    return true;
+                };
+            var longOp = () =>
+                {
+                    Task.Delay(700).Wait();
+                    return true;
+                };
+
+            var shortOpResult = shortOp.RunWithTimeout(500);
+            var longOpResult = longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void TestAsyncOperationBoolResultTimeout()
+        {
+            var shortOp = async () =>
+            {
+                await Task.Delay(200);
+                return true;
+            };
+            var longOp = async () =>
+            {
+                await Task.Delay(700);
+                return true;
+            };
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void OperationResultTimeout()
+        {
+            var shortOp = () => ResultExtensions.AsResult(
+                () =>
+                {
+                    Task.Delay(200).Wait();
+                    return true;
+                });
+
+            var longOp = () => ResultExtensions.AsResult(
+                () =>
+                {
+                    Task.Delay(700).Wait();
+                    return true;
+                });
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+
+        [Fact]
+        public async void TestAsyncOperationResultTimeout()
+        {
+            var shortOp = async () => await ResultExtensions.AsResult(
+                async () =>
+                {
+                    await Task.Delay(200);
+                    return true;
+                });
+
+            var longOp = async () => await ResultExtensions.AsResult(
+                async () =>
+                {
+                    await Task.Delay(700);
+                    return true;
+                });
+
+            var shortOpResult = await shortOp.RunWithTimeout(500);
+            var longOpResult = await longOp.RunWithTimeout(500);
+
+            Assert.True(shortOpResult.IsSuccess);
+
+            Assert.True(longOpResult.IsFailure);
+        }
+    }
+}
---
+++ FunctionalExtensions/FunctionalExtensions.Base/Timing.cs
@@ -0,0 +1,220 @@
+﻿using FunctionalExtensions.Base.Results;
+using static FunctionalExtensions.Base.Try;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtensions.Base
+{
+    public static class Timing
+    {
+        /// <summary>
+        /// Run an operation within a time limit
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="operation">Limited operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Data result of operation</returns>
+        public static DataResult<R> RunWithTimeout<R>(this Func<R> operation, int timeoutMillis)
+            => TryCatch(
+                () =>
+                {
+                    R result = default;
+                    bool finishedBeforeTimeout = Task.Run(() => result = operation()).Wait(timeoutMillis);
+                    return (finishedBeforeTimeout, result);
+                },
+                _ => _
+                ).ToDataResult()
+                .Bind(_ => _.finishedBeforeTimeout
+                            ? ResultExtensions.AsDataResult(() => _.result)
+                            : DataResult<R>.OnFail<R>("Timeout reached"));
+
+        /// <summary>
+        /// Run an async operation within a time limit 
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="operation">Limited async operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Data result of operation</returns>
+        public static async Task<DataResult<R>> RunWithTimeout<R>(this Func<Task<R>> operation, int timeoutMillis)
+            => await ResultExtensions.AsDataResult(
+                async () =>
+                {
+                    R result = default;
+                    var task = async () => result = await operation();
+                    var delayTask = Task.Delay(timeoutMillis);
+                    var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                    if (completedTask != delayTask)
+                    {
+                        return DataResult<R>.OnSuccess(result);
+                    }
+                    else
+                    {
+                        return DataResult<R>.OnFail<R>("Timeout reached");
+                    }
+                }).Map(_ => _.Data);
+
+        /// <summary>
+        /// Run an operation within a time limit
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="operation">Limited operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Data result of operation</returns>
+        public static async Task<DataResult<R>> RunWithTimeout<R>(this Func<DataResult<R>> operation, int timeoutMillis)
+            => await ResultExtensions.AsDataResult(
+                async () =>
+                {
+                    DataResult<R> result = default;
+                    var task = async () => result = await Task.Run(() => operation());
+                    var delayTask = Task.Delay(timeoutMillis);
+                    var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                    if (completedTask != delayTask)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        return DataResult<R>.OnFail<R>("Timeout reached");
+                    }
+                }).Map(_ => _.Data);
+
+        /// <summary>
+        /// Run an operation within a time limit
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="operation">Limited async operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Data result of operation</returns>
+        public static async Task<DataResult<R>> RunWithTimeout<R>(this Func<Task<DataResult<R>>> operation, int timeoutMillis)
+            => await ResultExtensions.AsDataResult(
+                async () =>
+                {
+                    DataResult<R> result = default;
+                    var task = async () => result = await operation();
+                    var delayTask = Task.Delay(timeoutMillis);
+                    var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                    if (completedTask != delayTask)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        return DataResult<R>.OnFail<R>("Timeout reached");
+                    }
+                }).Map(_ => _.Data);
+
+        /// <summary>
+        /// Run an operation within a time limit
+        /// </summary>
+        /// <param name="operation">Limited operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Result of operation</returns>
+        public static Result RunWithTimeout(this Func<bool> operation, int timeoutMillis)
+        {
+            try
+            {
+                bool result = false;
+                bool finishedBeforeTimeout = Task.Run(() => result = operation()).Wait(timeoutMillis);
+                return finishedBeforeTimeout
+                    ? ResultExtensions.AsResult(() => result)
+                    : Result.OnFail("Timeout reached");
+
+            }
+            catch (Exception exception)
+            {
+                return Result.OnException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Run an operatoin within a time limit
+        /// </summary>
+        /// <param name="operation">Limited async operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Result of operation</returns>
+        public async static Task<Result> RunWithTimeout(this Func<Task<bool>> operation, int timeoutMillis)
+        {
+            try
+            {
+                bool result = false;
+                var task = async () => result = await operation();
+                var delayTask = Task.Delay(timeoutMillis);
+                var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                if (completedTask != delayTask)
+                {
+                    return ResultExtensions.AsResult(() => result);
+                }
+                else
+                {
+                    return Result.OnFail("Timeout reached");
+                }
+            }
+            catch (Exception exception)
+            {
+                return Result.OnException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Run an operatoin within a time limit
+        /// </summary>
+        /// <param name="operation">Limited operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Result of operation</returns>
+        public static async Task<Result> RunWithTimeout(this Func<Result> operation, int timeoutMillis)
+        {
+            try
+            {
+                Result result = default;
+                var task = async () => result = await Task.Run(() => operation());
+                var delayTask = Task.Delay(timeoutMillis);
+                var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                if (completedTask != delayTask)
+                {
+                    return result;
+                }
+                else
+                {
+                    return Result.OnFail("Timeout reached");
+                }
+            }
+            catch (Exception exception)
+            {
+                return Result.OnException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Run an operatoin within a time limit
+        /// </summary>
+        /// <param name="operation">Limited async operation</param>
+        /// <param name="timeoutMillis">Time limit in milliseconds</param>
+        /// <returns>Result of operation</returns>
+        public static async Task<Result> RunWithTimeout(this Func<Task<Result>> operation, int timeoutMillis)
+        {
+            try
+            {
+                Result result = default;
+                var task = async () => result = await operation();
+                var delayTask = Task.Delay(timeoutMillis);
+                var completedTask = await Task.WhenAny(task.Invoke(), delayTask);
+                if (completedTask != delayTask)
+                {
+                    return result;
+                }
+                else
+                {
+                    return Result.OnFail("Timeout reached");
+                }
+            }
+            catch (Exception exception)
+            {
+                return Result.OnException(exception);
+            }
+        }
+
+    }
+}
