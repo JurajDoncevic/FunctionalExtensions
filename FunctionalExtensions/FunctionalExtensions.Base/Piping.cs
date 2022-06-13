1321203143d929a5c8aed4cc958bad29327a4c5a
JurajDoncevic <juraj.doncevic@fer.hr>
2022-06-13 12:44:28 +0200

    Added action for Pass extension

--- FunctionalExtensions/FunctionalExtensions.Base/Piping.cs
+++ FunctionalExtensions/FunctionalExtensions.Base/Piping.cs
@@ -8,6 +8,21 @@ namespace FunctionalExtensions.Base
 {
     public static class Piping
     {
+
+        /// <summary>
+        /// Executes an Action returning/side-effect function with target as the parameter and passes the target onward in the pipeline.
+        /// Side effects preserved if done on a mutable-type target (class)! 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target">Target object</param>
+        /// <param name="action">Side-effect action</param>
+        /// <returns></returns>
+        public static T Pass<T>(this T target, Action<T> action)
+        {
+            action(target);
+            return target;
+        }
+
         /// <summary>
         /// Executes a Unit returning/side-effect function with target as the parameter and passes the target onward in the pipeline.
         /// Side effects preserved if done on a mutable-type target (class)! 
