7f7e65f0df22a5e7d0d60c93a6752fabf76de057
JurajDoncevic <24619666+JurajDoncevic@users.noreply.github.com>
2022-06-17 22:13:53 +0200

    Removed unkept projects

--- FunctionalExtensions/FunctionalExtensions.GenericProvider.Tests/Data/FalseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FunctionalExtensions.GenericProvider.Tests.Data
-{
-    public class FalseModel : BaseModel<int> { }
-}
